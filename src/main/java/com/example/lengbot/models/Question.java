11a0982dc2314ac2f9861618a1ea4e7a4263abe1
arthur noire <demidmeleshin@mail.ru>
2022-09-21 22:15:24 +0500

    1. Добавил класс модели вопроса.

---
+++ src/main/java/com/example/lengbot/models/Question.java
@@ -0,0 +1,27 @@
+package com.example.lengbot.models;
+
+import java.util.List;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Question {
+    private String text;
+    private List<String> possibleAnswers;
+    private String rightAnswer;
+    private int weight;
+
+    public Question() {
+
+    }
+
+    public Question(String text, List<String> possibleAnswers, String rightAnswer, int weight) {
+        this.text = text;
+        this.possibleAnswers = possibleAnswers;
+        this.rightAnswer = rightAnswer;
+        this.weight = weight;
+    }
+
+}
