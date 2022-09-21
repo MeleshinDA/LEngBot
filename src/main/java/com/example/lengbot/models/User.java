e7e819245dd5f067d2d6cab439c68c2e76dbb7cb
arthur noire <demidmeleshin@mail.ru>
2022-09-21 22:08:03 +0500

    1. Добавил класс модели пользователя.

---
+++ src/main/java/com/example/lengbot/models/User.java
@@ -0,0 +1,23 @@
+package com.example.lengbot.models;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class User {
+    private String id;
+    private String lvl;
+
+    public User()
+    {
+
+    }
+    public User(String id, String lvl)
+    {
+        this.id = id;
+        this.lvl = lvl;
+    }
+
+}
