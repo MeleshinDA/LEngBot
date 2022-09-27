85d8dd4b086ea1824c76fe0b95e9d9253a9d1aa2
arthur noire <demidmeleshin@mail.ru>
2022-09-27 19:12:06 +0500

    1. Некрасивое, но решение проблемы состояний бота.

--- src/main/java/com/example/lengbot/telegram/handlers/MessageHandler.java
+++ src/main/java/com/example/lengbot/telegram/handlers/MessageHandler.java
@@ -2,56 +2,81 @@ package com.example.lengbot.telegram.handlers;
 
 import com.example.lengbot.API.HandlersAPI;
 import com.example.lengbot.constants.BotMessageEnum;
-import com.example.lengbot.constants.ButtonNameEnum;
-import com.example.lengbot.dao.QuestionDAO;
-import com.example.lengbot.dao.UserDAO;
 import com.example.lengbot.models.Question;
 import com.example.lengbot.telegram.TelegramApiClient;
 import com.example.lengbot.telegram.keyboards.InlineKeyboardMaker;
 import com.example.lengbot.telegram.keyboards.ReplyKeyboardMaker;
 import lombok.AllArgsConstructor;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 
-import java.util.LinkedList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 
 @Component
 @AllArgsConstructor
 public class MessageHandler {
 
-    //private Boolean isTesting = false;
-
-
+    private Boolean isTesting = false;
+    private Boolean isEnteringLvl = false;
     ReplyKeyboardMaker replyKeyboardMaker;
     InlineKeyboardMaker inlineKeyboardMaker;
-    UserDAO userDAO;
-    QuestionDAO questionDAO;
 
-    //HandlersAPI handlersAPI;
+    HandlersAPI handlersAPI;
+    Question curQuestion;
+    private int scores;
 
     public BotApiMethod<?> answerMessage(Message message){
         String chatId = message.getChatId().toString();
         String inputText = message.getText();
 
-        /*if (isTesting)
-            return getTestMessages(chatId);*/
-        return switch (inputText){
+        if (isTesting)
+        {
+            if (curQuestion.getRightAnswer().equals(inputText))
+                scores += curQuestion.getWeight();
+            return getTestMessages(chatId);
+        }
+
+        if (isEnteringLvl)
+        {
+            Set<String> rightLvls = new HashSet<String>();
+            rightLvls.add("A0");
+            rightLvls.add("A1");
+            rightLvls.add("A2");
+            rightLvls.add("B1");
+            rightLvls.add("B2");
+
+            if (rightLvls.contains(inputText.toUpperCase()))
+            {
+                handlersAPI.userDAO.UpdateUser(Integer.parseInt(chatId), inputText.toUpperCase());
+                isEnteringLvl = false;
+                return new SendMessage(chatId, "Уровень сохранён");
+            }
+
+            return new SendMessage(chatId, "Неправильно введён уровень, доступные варианты: A0, A1, A2, B1, B2");
+        }
+
+
+        return switch (inputText) {
             case null -> throw new IllegalArgumentException();
             case "/start" -> getStartMessage(chatId);
-            case "Пройти тест" -> getTestMessages(chatId);
-                //isTesting = true;
-            case "Ввести уровень" -> getLevelMessages(chatId);
+            case "Пройти тест" -> {
+                isTesting = true;
+                yield getTestMessages(chatId);
+            }
+            case "Ввести уровень" -> {
+                isEnteringLvl = true;
+                yield getLevelMessages(chatId);
+            }
             default -> new SendMessage(chatId, "Пожалуйста воспользуйтесь клавиатурой");
         };
     }
 
     private SendMessage getStartMessage(String chatId){
-        userDAO.SaveUser(Integer.parseInt(chatId));
+        handlersAPI.userDAO.SaveUser(Integer.parseInt(chatId));
         SendMessage sendMessage = new SendMessage(chatId, BotMessageEnum.HELP_MESSAGE.getMessage());
         sendMessage.enableMarkdown(true);
         sendMessage.setReplyMarkup(replyKeyboardMaker.getMainMenuKeyboard());
@@ -59,17 +84,7 @@ public class MessageHandler {
     }
 
     private SendMessage getTestMessages(String chatId){
-        List<Question> questions =  questionDAO.GetTest();
-        List<SendMessage> messages = new LinkedList<>();
-        for (Question question: questions){
-            SendMessage newMessage = new SendMessage(chatId, question.getText());
-            newMessage.setReplyMarkup(inlineKeyboardMaker.getInlineMessageButtons(" "));
-            messages.add(newMessage);
-        }
-        SendMessage newMessage = new SendMessage(chatId, questions.get(0).getText());
-        newMessage.setReplyMarkup(inlineKeyboardMaker.getInlineMessageButtons(" "));
-        messages.add(newMessage);
-        /*Question curQuestion = handlersAPI.GetNextQuestion();
+        curQuestion = handlersAPI.GetNextQuestion();
         SendMessage sendMessage = new SendMessage(chatId, "");
         sendMessage.enableMarkdown(true);
         sendMessage.setReplyMarkup(replyKeyboardMaker.getMainMenuKeyboard());
@@ -82,11 +97,14 @@ public class MessageHandler {
         else
             sendMessage.setText(curQuestion.getText() + "\n" + curQuestion.getPossibleAnswers());
 
-        return sendMessage;*/
-        return newMessage;
+        return sendMessage;
     }
 
-    private SendMessage getLevelMessages(String chatId){
-        return null;
+    private SendMessage getLevelMessages(String chatId) {
+
+        SendMessage sendMessage = new SendMessage(chatId, "Введите уровень:");
+        sendMessage.enableMarkdown(true);
+        sendMessage.setReplyMarkup(replyKeyboardMaker.getMainMenuKeyboard());
+        return sendMessage;
     }
 }
