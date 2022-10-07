b517c8ddfee28bba9160ca8d503048925103fe4b
Olya <mshna.oi.2002@yandex.ru>
2022-10-07 21:05:51 +0500

    1. Добавлена рандомная генерация тестов.

--- src/main/java/com/example/lengbot/dao/QuestionDAO.java
+++ src/main/java/com/example/lengbot/dao/QuestionDAO.java
@@ -1,6 +1,9 @@
 package com.example.lengbot.dao;
 
 import com.example.lengbot.models.Question;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
@@ -13,11 +16,17 @@ import java.util.List;
 @Repository
 public class QuestionDAO {
 
+  private final Map<String, Integer> dbQuestionTypesToCount;
   private final JdbcTemplate jdbcTemplate;
 
   @Autowired
   public QuestionDAO(JdbcTemplate jdbcTemplate) {
     this.jdbcTemplate = jdbcTemplate;
+
+    this.dbQuestionTypesToCount = new LinkedHashMap<>();
+    this.dbQuestionTypesToCount.put("first", 8);
+    this.dbQuestionTypesToCount.put("second", 5);
+    this.dbQuestionTypesToCount.put("third", 2);
   }
 
   /**
@@ -26,6 +35,14 @@ public class QuestionDAO {
    * @return Готовый тест (список вопросов)
    */
   public List<Question> getTest() {
-    return jdbcTemplate.query("SELECT * FROM test", new QuestionMapper());
+    List<Question> test = new ArrayList<>();
+
+    for (String tableName : dbQuestionTypesToCount.keySet()) {
+      test.addAll(jdbcTemplate.query("SELECT * FROM ? ORDER BY RANDOM() LIMIT ?",
+          new Object[]{tableName, dbQuestionTypesToCount.get(tableName)},
+          new QuestionMapper()));
+    }
+
+    return test;
   }
 }
